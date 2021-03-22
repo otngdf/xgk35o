a1cced99ee029dac6e509f35b87435441f03b047
User <User@Z10>
2021-03-22 22:43:10 +0100

    get > post

--- src/main/java/org/zgdf/ea/controller/UsersCreateController.java
+++ src/main/java/org/zgdf/ea/controller/UsersCreateController.java
@@ -11,7 +11,7 @@ import org.zgdf.ea.utils.HashPassword;
 public class UsersCreateController extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         String uname = request.getParameter("uname");
