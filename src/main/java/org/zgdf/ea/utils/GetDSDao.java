a05824a5a22591edf47bc4e1f2d71c7690133816
User <User@Z10>
2021-06-20 12:59:04 +0200

    nk

--- src/main/java/org/zgdf/ea/utils/GetDSDao.java
+++ src/main/java/org/zgdf/ea/utils/GetDSDao.java
@@ -15,7 +15,7 @@ public class GetDSDao {
     public GetDSDao() throws NamingException {
         this.ctx = new InitialContext();
         //this.ds = (DataSource) ctx.lookup("java:/appDS");
-        this.ds = (DataSource) ctx.lookup("java:/MunkaidoDB");
+        this.ds = (DataSource) ctx.lookup("java:/MunkaidoDS");
     }
 
     /**
